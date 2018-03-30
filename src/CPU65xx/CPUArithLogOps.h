c0f6ed1b2df188bb8a99f20b6bdb591bcbda762b
Dumonu <jacobcherry36@gmail.com>
2018-03-30 16:41:18 -0500

    Refactored CPUArithLogOps.h and CPURotShift.h

--- src/CPU65xx/CPUArithLogOps.h
+++ src/CPU65xx/CPUArithLogOps.h
@@ -8,106 +8,121 @@
 // .................................................
 // ADD MEMORY TO ACCUMULATOR WITH CARRY
 // .................................................
-uint8_t ADC(uint8_t op, uint8_t arg0, uint8_t arg1);    // ADC #nn
-                                                        // ADC nn
-                                                        // ADC nn,X
-                                                        // ADC nnnn
-                                                        // ADC nnnn,X
-                                                        // ADC nnnn,Y
-                                                        // ADC (nn,X)
-                                                        // ADC (nn),Y
+uint8_t ADC_69(uint8_t arg0, uint8_t arg1);    // ADC #nn
+uint8_t ADC_65(uint8_t arg0, uint8_t arg1);    // ADC nn
+uint8_t ADC_75(uint8_t arg0, uint8_t arg1);    // ADC nn,X
+uint8_t ADC_6D(uint8_t arg0, uint8_t arg1);    // ADC nnnn
+uint8_t ADC_7D(uint8_t arg0, uint8_t arg1);    // ADC nnnn,X
+uint8_t ADC_79(uint8_t arg0, uint8_t arg1);    // ADC nnnn,Y
+uint8_t ADC_61(uint8_t arg0, uint8_t arg1);    // ADC (nn,X)
+uint8_t ADC_71(uint8_t arg0, uint8_t arg1);    // ADC (nn),Y
 
 // .................................................
 // SUBTRACT MEMORY FROM ACCUMULATOR WITH BORROW
 // .................................................
-uint8_t SBC(uint8_t op, uint8_t arg0, uint8_t arg1);    // SBC #nn
-                                                        // SBC nn
-                                                        // SBC nn,X
-                                                        // SBC nnnn
-                                                        // SBC nnnn,X
-                                                        // SBC nnnn,Y
-                                                        // SBC (nn,X)
-                                                        // SBC (nn),Y
+uint8_t SBC_E9(uint8_t arg0, uint8_t arg1);    // SBC #nn
+uint8_t SBC_E5(uint8_t arg0, uint8_t arg1);    // SBC nn
+uint8_t SBC_F5(uint8_t arg0, uint8_t arg1);    // SBC nn,X
+uint8_t SBC_ED(uint8_t arg0, uint8_t arg1);    // SBC nnnn
+uint8_t SBC_FD(uint8_t arg0, uint8_t arg1);    // SBC nnnn,X
+uint8_t SBC_F9(uint8_t arg0, uint8_t arg1);    // SBC nnnn,Y
+uint8_t SBC_E1(uint8_t arg0, uint8_t arg1);    // SBC (nn,X)
+uint8_t SBC_F1(uint8_t arg0, uint8_t arg1);    // SBC (nn),Y
 
 // .................................................
 // LOGICAL AND MEMORY WITH ACCUMULATOR
 // .................................................
-uint8_t AND(uint8_t op, uint8_t arg0, uint8_t arg1);    // AND #nn
-                                                        // AND nn
-                                                        // AND nn,X
-                                                        // AND nnnn
-                                                        // AND nnnn,X
-                                                        // AND nnnn,Y
-                                                        // AND (nn,X)
-                                                        // AND (nn),Y
+uint8_t AND_29(uint8_t arg0, uint8_t arg1);    // AND #nn
+uint8_t AND_25(uint8_t arg0, uint8_t arg1);    // AND nn
+uint8_t AND_35(uint8_t arg0, uint8_t arg1);    // AND nn,X
+uint8_t AND_2D(uint8_t arg0, uint8_t arg1);    // AND nnnn
+uint8_t AND_3D(uint8_t arg0, uint8_t arg1);    // AND nnnn,X
+uint8_t AND_39(uint8_t arg0, uint8_t arg1);    // AND nnnn,Y
+uint8_t AND_21(uint8_t arg0, uint8_t arg1);    // AND (nn,X)
+uint8_t AND_31(uint8_t arg0, uint8_t arg1);    // AND (nn),Y
 
 // .................................................
 // EXCLUSIVE-OR MEMORY WITH ACCUMULATOR
 // .................................................
-uint8_t EOR(uint8_t op, uint8_t arg0, uint8_t arg1);    // EOR #nn
-                                                        // EOR nn
-                                                        // EOR nn,X
-                                                        // EOR nnnn
-                                                        // EOR nnnn,X
-                                                        // EOR nnnn,Y
-                                                        // EOR (nn,X)
-                                                        // EOR (nn),Y
+uint8_t EOR_49(uint8_t arg0, uint8_t arg1);    // EOR #nn
+uint8_t EOR_45(uint8_t arg0, uint8_t arg1);    // EOR nn
+uint8_t EOR_55(uint8_t arg0, uint8_t arg1);    // EOR nn,X
+uint8_t EOR_4D(uint8_t arg0, uint8_t arg1);    // EOR nnnn
+uint8_t EOR_5D(uint8_t arg0, uint8_t arg1);    // EOR nnnn,X
+uint8_t EOR_59(uint8_t arg0, uint8_t arg1);    // EOR nnnn,Y
+uint8_t EOR_41(uint8_t arg0, uint8_t arg1);    // EOR (nn,X)
+uint8_t EOR_51(uint8_t arg0, uint8_t arg1);    // EOR (nn),Y
                                                         
 // .................................................
 // LOGICAL OR MEMORY WITH ACCUMULATOR
 // .................................................
-uint8_t ORA(uint8_t op, uint8_t arg0, uint8_t arg1);    // ORA #nn
-                                                        // ORA nn
-                                                        // ORA nn,X
-                                                        // ORA nnnn
-                                                        // ORA nnnn,X
-                                                        // ORA nnnn,Y
-                                                        // ORA (nn,X)
-                                                        // ORA (nn),Y
+uint8_t ORA_09(uint8_t arg0, uint8_t arg1);    // ORA #nn
+uint8_t ORA_05(uint8_t arg0, uint8_t arg1);    // ORA nn
+uint8_t ORA_15(uint8_t arg0, uint8_t arg1);    // ORA nn,X
+uint8_t ORA_0D(uint8_t arg0, uint8_t arg1);    // ORA nnnn
+uint8_t ORA_1D(uint8_t arg0, uint8_t arg1);    // ORA nnnn,X
+uint8_t ORA_19(uint8_t arg0, uint8_t arg1);    // ORA nnnn,Y
+uint8_t ORA_01(uint8_t arg0, uint8_t arg1);    // ORA (nn,X)
+uint8_t ORA_11(uint8_t arg0, uint8_t arg1);    // ORA (nn),Y
 
 // .................................................
 // COMPARE
 // .................................................
-uint8_t CMP(uint8_t op, uint8_t arg0, uint8_t arg1);    // CMP #nn
-                                                        // CMP nn
-                                                        // CMP nn,X
-                                                        // CMP nnnn
-                                                        // CMP nnnn,X
-                                                        // CMP nnnn,Y
-                                                        // CMP (nn,X)
-                                                        // CMP (nn),Y
-uint8_t CPX(uint8_t op, uint8_t arg0, uint8_t arg1);    // CPX #nn
-                                                        // CPX nn
-                                                        // CPX nnnn
-uint8_t CPY(uint8_t op, uint8_t arg0, uint8_t arg1);    // CPY #nn
-                                                        // CPY nn
-                                                        // CPY nnnn
+// Compare A with Memory or Immediate
+uint8_t CMP_C9(uint8_t arg0, uint8_t arg1);    // CMP #nn
+uint8_t CMP_C5(uint8_t arg0, uint8_t arg1);    // CMP nn
+uint8_t CMP_D5(uint8_t arg0, uint8_t arg1);    // CMP nn,X
+uint8_t CMP_CD(uint8_t arg0, uint8_t arg1);    // CMP nnnn
+uint8_t CMP_DD(uint8_t arg0, uint8_t arg1);    // CMP nnnn,X
+uint8_t CMP_D9(uint8_t arg0, uint8_t arg1);    // CMP nnnn,Y
+uint8_t CMP_C1(uint8_t arg0, uint8_t arg1);    // CMP (nn,X)
+uint8_t CMP_D1(uint8_t arg0, uint8_t arg1);    // CMP (nn),Y
+
+// Compare X with Memory or Immediate
+uint8_t CPX_E0(uint8_t arg0, uint8_t arg1);    // CPX #nn
+uint8_t CPX_E4(uint8_t arg0, uint8_t arg1);    // CPX nn
+uint8_t CPX_EC(uint8_t arg0, uint8_t arg1);    // CPX nnnn
+
+// Compare Y with Memory or Immediate
+uint8_t CPY_C0(uint8_t arg0, uint8_t arg1);    // CPY #nn
+uint8_t CPY_C4(uint8_t arg0, uint8_t arg1);    // CPY nn
+uint8_t CPY_CC(uint8_t arg0, uint8_t arg1);    // CPY nnnn
 
 // .................................................
 // BIT TEST
 // .................................................
-uint8_t BIT(uint8_t op, uint8_t arg0, uint8_t arg1);    // BIT nn
-                                                        // BIT nnnn
+uint8_t BIT_24(uint8_t arg0, uint8_t arg1);    // BIT nn
+uint8_t BIT_2C(uint8_t arg0, uint8_t arg1);    // BIT nnnn
 
 // .................................................
 // INCREMENT BY ONE
 // .................................................
-uint8_t INC(uint8_t op, uint8_t arg0, uint8_t arg1);    // INC nn
-                                                        // INC nn,X
-                                                        // INC nnnn
-                                                        // INC nnnn,X
-uint8_t INX(uint8_t op, uint8_t arg0, uint8_t arg1);    // INX
-uint8_t INY(uint8_t op, uint8_t arg0, uint8_t arg1);    // INY
+// Increment memory
+uint8_t INC_E6(uint8_t arg0, uint8_t arg1);    // INC nn
+uint8_t INC_F6(uint8_t arg0, uint8_t arg1);    // INC nn,X
+uint8_t INC_EE(uint8_t arg0, uint8_t arg1);    // INC nnnn
+uint8_t INC_FE(uint8_t arg0, uint8_t arg1);    // INC nnnn,X
+
+// Increment X
+uint8_t INX_E8(uint8_t arg0, uint8_t arg1);    // INX
+
+// Increment Y
+uint8_t INY_C8(uint8_t arg0, uint8_t arg1);    // INY
 
 // .................................................
 // DECREMENT BY ONE
 // .................................................
-uint8_t DEC(uint8_t op, uint8_t arg0, uint8_t arg1);    // DEC nn
-                                                        // DEC nn,X
-                                                        // DEC nnnn
-                                                        // DEC nnnn,X
-uint8_t DEX(uint8_t op, uint8_t arg0, uint8_t arg1);    // DEX
-uint8_t DEY(uint8_t op, uint8_t arg0, uint8_t arg1);    // DEY
+// Decrement memory
+uint8_t DEC_C6(uint8_t arg0, uint8_t arg1);    // DEC nn
+uint8_t DEC_D6(uint8_t arg0, uint8_t arg1);    // DEC nn,X
+uint8_t DEC_CE(uint8_t arg0, uint8_t arg1);    // DEC nnnn
+uint8_t DEC_DE(uint8_t arg0, uint8_t arg1);    // DEC nnnn,X
+
+// Decrement X
+uint8_t DEX_CA(uint8_t arg0, uint8_t arg1);    // DEX
+
+// Decrement Y
+uint8_t DEY_88(uint8_t arg0, uint8_t arg1);    // DEY
 
 
 #endif
--- src/CPU65xx/CPURotShift.h
+++ src/CPU65xx/CPURotShift.h
@@ -8,38 +8,38 @@
 // .................................................
 // SHIFT LEFT LOGICAL/ARITHMETIC
 // .................................................
-uint8_t ASL(uint8_t op, uint8_t arg0, uint8_t arg1);    // ASL A
-                                                        // ASL nn
-                                                        // ASL nn,X
-                                                        // ASL nnnn
-                                                        // ASL nnnn,X
+uint8_t ASL_0A(uint8_t arg0, uint8_t arg1);    // ASL A
+uint8_t ASL_06(uint8_t arg0, uint8_t arg1);    // ASL nn
+uint8_t ASL_16(uint8_t arg0, uint8_t arg1);    // ASL nn,X
+uint8_t ASL_0E(uint8_t arg0, uint8_t arg1);    // ASL nnnn
+uint8_t ASL_1E(uint8_t arg0, uint8_t arg1);    // ASL nnnn,X
 
 // .................................................
 // SHIFT RIGHT LOGICAL
 // .................................................
-uint8_t LSR(uint8_t op, uint8_t arg0, uint8_t arg1);    // LSR A
-                                                        // LSR nn
-                                                        // LSR nn,X
-                                                        // LSR nnnn
-                                                        // LSR nnnn,X
+uint8_t LSR_4A(uint8_t arg0, uint8_t arg1);    // LSR A
+uint8_t LSR_46(uint8_t arg0, uint8_t arg1);    // LSR nn
+uint8_t LSR_56(uint8_t arg0, uint8_t arg1);    // LSR nn,X
+uint8_t LSR_4E(uint8_t arg0, uint8_t arg1);    // LSR nnnn
+uint8_t LSR_5E(uint8_t arg0, uint8_t arg1);    // LSR nnnn,X
 
 // .................................................
 // ROTATE LEFT THROUGH CARRY
 // .................................................
-uint8_t ROL(uint8_t op, uint8_t arg0, uint8_t arg1);    // ROL A
-                                                        // ROL nn
-                                                        // ROL nn,X
-                                                        // ROL nnnn
-                                                        // ROL nnnn,X
+uint8_t ROL_2A(uint8_t arg0, uint8_t arg1);    // ROL A
+uint8_t ROL_26(uint8_t arg0, uint8_t arg1);    // ROL nn
+uint8_t ROL_36(uint8_t arg0, uint8_t arg1);    // ROL nn,X
+uint8_t ROL_2E(uint8_t arg0, uint8_t arg1);    // ROL nnnn
+uint8_t ROL_3E(uint8_t arg0, uint8_t arg1);    // ROL nnnn,X
 
 // .................................................
 // ROTATE RIGHT THROUGH CARRY
 // .................................................
-uint8_t ROR(uint8_t op, uint8_t arg0, uint8_t arg1);    // ROR A
-                                                        // ROR nn
-                                                        // ROR nn,X
-                                                        // ROR nnnn
-                                                        // ROR nnnn,X
+uint8_t ROR_6A(uint8_t arg0, uint8_t arg1);    // ROR A
+uint8_t ROR_66(uint8_t arg0, uint8_t arg1);    // ROR nn
+uint8_t ROR_76(uint8_t arg0, uint8_t arg1);    // ROR nn,X
+uint8_t ROR_6E(uint8_t arg0, uint8_t arg1);    // ROR nnnn
+uint8_t ROR_7E(uint8_t arg0, uint8_t arg1);    // ROR nnnn,X
 
 
 #endif
